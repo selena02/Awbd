bbeb62dacded3501b9d499092b0b32ac8818e881
Selena Hurloi <88150796+selena02@users.noreply.github.com>
2025-09-06 12:43:53 +0300

    Delete src directory

--- src/main/java/com/example/springbootprojectuni/exception/CartNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootprojectuni.exception;
-
-public class CartNotFoundException extends RuntimeException{
-    public CartNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/springbootprojectuni/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootprojectuni.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
